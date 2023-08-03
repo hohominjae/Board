c2752246cec366a25d1e5abab7fedaa959bb31ac
hohominjae <jmj0531@naver.com>
2023-08-03 16:29:08 +0900

    user signup login 구현완료 테스트 전 저장

--- src/main/java/com/sparta/boardlv3/user/controller/UserController.java
+++ src/main/java/com/sparta/boardlv3/user/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.sparta.boardlv3.user.controller;
 
+import com.sparta.boardlv3.common.api.ApiResponseDto;
+import com.sparta.boardlv3.common.jwt.JwtUtil;
+import com.sparta.boardlv3.user.dto.AuthRequestDto;
+import com.sparta.boardlv3.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
 public class UserController {
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponseDto> signUp(@RequestBody AuthRequestDto authRequestDto) {
+
+        try {
+            userService.signup(authRequestDto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto("중복된 Id 입니다.", HttpStatus.BAD_REQUEST.value()));
+        }
+
+        return ResponseEntity.ok().body(new ApiResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDto> login(@RequestBody AuthRequestDto authRequestDto, HttpServletResponse servletResponse) {
+        try {
+            userService.login(authRequestDto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto("회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()));
+        }
+
+        servletResponse.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(authRequestDto.getUsername(), authRequestDto.getRole()));
+
+        return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
+    }
 }
--- src/main/java/com/sparta/boardlv3/user/service/UserService.java
+++ src/main/java/com/sparta/boardlv3/user/service/UserService.java
@@ -1,4 +1,43 @@
 package com.sparta.boardlv3.user.service;
 
+import com.sparta.boardlv3.user.dto.AuthRequestDto;
+import com.sparta.boardlv3.user.entity.User;
+import com.sparta.boardlv3.user.entity.UserRoleEnum;
+import com.sparta.boardlv3.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    public void signup(AuthRequestDto authRequestDto) {
+        String username = authRequestDto.getUsername();
+        String password = passwordEncoder.encode(authRequestDto.getPassword());
+        UserRoleEnum role = authRequestDto.getRole();
+
+        if (userRepository.findByUsername(username).isPresent()){
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+
+        User user = new User(username, password, role);
+        userRepository.save(user);
+    }
+
+    public void login(AuthRequestDto authRequestDto) {
+        String username = authRequestDto.getUsername();
+        String password = authRequestDto.getPassword();
+
+        //Id 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("Id가 틀렸습니다.")
+        );
+
+        //패스워드 확인
+        if (passwordEncoder.matches(password, user.getPassword())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
 }
